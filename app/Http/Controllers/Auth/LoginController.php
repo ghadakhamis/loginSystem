5828bb9fd8687d14fc149b51630518fbeaa39a2d
ghadakhamis <ghada.khamis.m@gmail.com>
2018-07-23 19:29:06 +0200

    allow users to login with email or mobile number

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Auth;
 
 class LoginController extends Controller
 {
@@ -35,5 +37,25 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function login(Request $request){
+        
+        // request validation 
+        $this->validate($request, [
+            'emailORmobile'    => 'required',
+            'password' => 'required',
+        ]);
+
+        // defined field which user logged in
+        $field = filter_var($request->emailORmobile, FILTER_VALIDATE_EMAIL) ? 'email' : 'mobile_number';
+        
+        // change parameter in the request to field type which user logged in
+        $request->merge([$field => $request->emailORmobile]);
+
+        if (Auth::attempt($request->only($field, 'password'))) {
+            return redirect()->intended($this->redirectTo);
+        } 
+        return redirect()->back()->withInput()->withErrors([ 'emailORmobile' => "These credentials do not match our records." ]);
     }
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,14 +12,14 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="emailORmobile" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address / Mobile') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+                                <input id="emailORmobile" type="text" class="form-control{{ $errors->has('emailORmobile') ? ' is-invalid' : '' }}" name="emailORmobile" value="{{ old('emailORmobile') }}" required autofocus>
 
-                                @if ($errors->has('email'))
+                                @if ($errors->has('emailORmobile'))
                                     <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
+                                        <strong>{{ $errors->first('emailORmobile') }}</strong>
                                     </span>
                                 @endif
                             </div>
